3c20ea31b440543c10a5c1bf8c39e59c14f0f3f0
prince verma <75908342+Princevermacoldreaper@users.noreply.github.com>
2024-11-17 17:53:34 +0530

    5th day

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,37 @@
 import RestaurentCard from "./RestaurentCard";
-import resObj from "../utils/mockData";
 import { useState ,useEffect} from "react";
 
 const Body = () => {
-  const [ListOfRestaurents, setListOfRestaurents] = useState(resObj);
+  const [listOfRestaurents, setListOfRestaurents] = useState([]);
 
   //console.log(ListOfRestaurents);
-  useEffect(()=>{fetchData()},[]);
-  const fetchData=()=>{
-    const data=fetch()
+  useEffect(()=> {
+    const init = async() => {
+        const m = await fetchData()
+        setListOfRestaurents(...m)
+    }
+    init()  
+  },[]);
+
+  const fetchData=async()=>{
+    const API_URL="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.582574171253334&lng=77.40395203446887&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const data=await fetch(API_URL);
+    const json=await data.json();
+    // const restaur = json?.data?.cards?.find(card => card?.cardType === "whats_on_your_mind")?.data?.data?.cards;
+   return (json.data.cards.map((card) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants)).filter((k) => typeof k !== 'undefined')
+    
+   // console.log(json);
+    // console.log(json);
   }
+
+  console.log("TEST", listOfRestaurents)
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            const FilteredList = ListOfRestaurents.filter(
+            const FilteredList = listOfRestaurents.filter(
               (res) => res.info.avgRating > 4.4
             );
             //console.log(FilteredList ? FilteredList : "error");
@@ -26,11 +41,15 @@ const Body = () => {
           Top Rated Restaurants
         </button>
       </div>
-      <div className="res-container">
-        {ListOfRestaurents.map((restaurent) => (
-          <RestaurentCard key={restaurent.info.id} resData={restaurent} />
-        ))}
-      </div>
+      {listOfRestaurents.length > 0 && (
+
+<div className="res-container">
+{listOfRestaurents.map((restaurent) => (
+  <RestaurentCard key={restaurent.info.id} resData={restaurent} />
+))}
+</div>
+      )}
+     
     </div>
   );
 };
