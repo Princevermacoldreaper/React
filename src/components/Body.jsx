a4ae9829f3a9c2e4d35275928d205d7df8d0bf39
prince verma <75908342+Princevermacoldreaper@users.noreply.github.com>
2024-11-19 08:33:53 +0530

    6th day

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,16 @@
 import RestaurentCard from "./RestaurentCard";
 import { useState ,useEffect} from "react";
-
+import Shimmer from "./Shimmer";
+let data;
 const Body = () => {
   const [listOfRestaurents, setListOfRestaurents] = useState([]);
-
+  const [searchText,setsearchText]=useState("")
   //console.log(ListOfRestaurents);
   useEffect(()=> {
     const init = async() => {
-        const m = await fetchData()
-        setListOfRestaurents(...m)
+        const data = await fetchData()
+        setListOfRestaurents(...data)
+        
     }
     init()  
   },[]);
@@ -17,17 +19,21 @@ const Body = () => {
     const API_URL="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.582574171253334&lng=77.40395203446887&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
     const data=await fetch(API_URL);
     const json=await data.json();
-    // const restaur = json?.data?.cards?.find(card => card?.cardType === "whats_on_your_mind")?.data?.data?.cards;
-   return (json.data.cards.map((card) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants)).filter((k) => typeof k !== 'undefined')
-    
-   // console.log(json);
-    // console.log(json);
-  }
-
+    return (json.data.cards.map((card) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants)).filter((k) => typeof k !== 'undefined')
+   }
+  
   console.log("TEST", listOfRestaurents)
-  return (
-    <div className="body">
+ // console.log(listOfRestaurents.filter((k)=>k.info.name==searchText))
+   
+  return ( listOfRestaurents.length==0?<Shimmer/>:
+    <div className="body">  
       <div className="filter">
+      <input type="text" className="search-box" value={searchText} onChange={(e)=>{setsearchText(e.target.value)}}/>
+      <button onClick={()=>{
+        const filteredRestaurent=listOfRestaurents.filter((k)=>k.info.name==searchText);
+       setListOfRestaurents(filteredRestaurent);
+       console.log(data)
+      }}>Search</button>
         <button
           className="filter-btn"
           onClick={() => {
